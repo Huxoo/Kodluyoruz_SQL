9353e56ff5156ae82682a5f17b9d10a944f3ee17
Huxoo <90395771+Huxoo@users.noreply.github.com>
2022-10-03 15:07:38 +0300

    Yeni Sorular Cevaplandırıldı.

---
+++ SQL Ödev 2/Soru1.sql
@@ -0,0 +1,5 @@
+--film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit
+--ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+
+Select * From film
+Where replacement_cost Between 12.99 And 16.99;
---
+++ SQL Ödev 2/Soru2.sql
@@ -0,0 +1,5 @@
+--actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope'
+--veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)9;
+
+Select first_name, last_name From actor
+Where first_name In('Penelope', 'Nick', 'Ed')
