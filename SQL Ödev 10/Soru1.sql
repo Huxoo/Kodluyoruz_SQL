ee37188eefe7e934212da0c2a9d6bb6dccb5cf41
Huxoo <90395771+Huxoo@users.noreply.github.com>
2022-10-08 13:06:23 +0300

    Delete SQL Ödev10

--- SQL Ödev 10/Soru1.sql
+++
@@ -1,6 +0,0 @@
--- city tablosu ile country tablosunda bulunan şehir (city) ve
--- ülke (country) isimlerini birlikte görebileceğimiz LEFT
--- JOIN sorgusunu yazınız.
-
-Select city, country From city
-Left Join country On country.country_id = city.country_id
